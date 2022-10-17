a5e9d951b1fcee35b83cd29819ef799485cb54ce
bujinlkham-hub <bujinlhamb@gmail.com>
2022-10-17 13:27:19 +0800

    sliding  panel added

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -28,30 +28,195 @@ class HomePage extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
      return _HomePage();
+     
   }
 }
 
-class _HomePage extends State<HomePage>{
-
-  double  _drawerIconSize = 24;
+class _HomePage extends State<HomePage> {
+  double _drawerIconSize = 24;
   double _drawerFontSize = 17;
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-      backgroundColor: Color.fromRGBO(28, 35, 38, 100),
-      body: _buildMap(),
+        body: SafeArea(
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: MediaQuery.of(context).size.height * 0.85,
+                floating: true,
+                pinned: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    "Parcel list",
+                  ),
+                  
+                  background: _buildMap(),
+                  
+                ),
+              ),
+            ];
+          },
+          body: Center(
+              child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [      
+                      Text(
+                        "Search field",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.black,
+                        
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                          TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0Xff302360),
+                          border:OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: BorderSide.none,
+                            ),
+                            hintText: "Search field",
+                            prefixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return ListTile(
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.asset('images/${geter()[index].image!}',
+                            height: 40),
+                      ),
+                      title: Text(
+                        geter()[index].name!,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(
+                        geter()[index].time!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      trailing: Text(
+                        geter()[index].fee!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 19,
+                          color:
+                              geter()[index].buy! ? Colors.red : Colors.green,
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: geter().length,
+                ),
+              )
+            ],
+          )),
      
-      // bottomNavigationBar: MyHomePage(),
-     
-
-
-    );
+        ),
+         appBar: _buildAppBar(context),
+      ),
+    ));
   }
- 
-
 }
+
+
+    AppBar _buildAppBar(context) {
+      return AppBar(
+        
+        actions: [
+        // ElevatedButton(
+        //     style: ElevatedButton.styleFrom(
+        //       padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+        //      shape: RoundedRectangleBorder(
+        //      borderRadius: BorderRadius.circular(4.0) 
+                  
+                 
+        //           ),
+        //       // style: ElevatedButton.styleFrom(
+        //       //   shape: const StadiumBorder(),
+        //       ),
+              
+        //       child: const Text('Pill '),
+        //       onPressed: () {},     
+        //     ),       
+     
+
+          Container(
+            margin: EdgeInsets.only( top: 16, right: 16,),
+            child: Stack(       
+              children: <Widget>[    
+              new IconButton(
+                icon: new Icon(
+                  Icons.arrow_downward_outlined,
+                ),
+                onPressed: () {
+              showModalBottomSheet<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: 200,
+                    color: Colors.amber,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Text('Modal BottomSheet'),
+                          ElevatedButton(
+                            child: const Text('Close BottomSheet'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+              ),
+                Positioned(
+                  right: 0,
+                  child: 
+                  Container(
+                    padding: EdgeInsets.all(1),
+                    child: Text( '5', style: TextStyle(color: Colors.white, fontSize: 8,), textAlign: TextAlign.center,),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        
+      );
+  }
 
 
 FlutterMap _buildMap() {
